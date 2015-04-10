339cba501b656f273c55d5ac91998c6700ef960c
Belousov A <mpmb.for.reg@gmail.com>
2015-04-10 09:32:02 +0800

    1

---
+++ SM/SM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SM
+//
+//  Created by Andrey Belousov on 10.04.15.
+//  Copyright (c) 2015 Andrey Belousov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SM/SM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SM
+//
+//  Created by Andrey Belousov on 10.04.15.
+//  Copyright (c) 2015 Andrey Belousov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
